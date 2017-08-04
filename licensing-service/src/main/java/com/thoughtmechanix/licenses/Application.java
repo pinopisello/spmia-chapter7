95ffe39a665a696c92fb9e8e19cfb4aaa8c2777b
glocon <glocon@nike>
2017-08-04 14:31:53 -0700

    update 4/8/17

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -32,10 +32,11 @@ public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
+    
+    
     @LoadBalanced
     @Bean
-    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext,
-                                                 OAuth2ProtectedResourceDetails details) {
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
         return new OAuth2RestTemplate(details, oauth2ClientContext);
     }
 
